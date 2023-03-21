67830ebbc9d82a57a9ccf5d1861194ccd950791b
oucyongdu <csyongdu@163.com>
2023-03-22 02:02:38 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: "Yong Du's Homepage",
   description: "Yong Du's Homepage",
   head: [["link", { rel: "icon", href: `/headlogo.png` }]],
-  /*base: "/homepage/",*/
+  base: "/homepage/",
   dest: ".vuepress/dist",
 
   themeConfig: {
